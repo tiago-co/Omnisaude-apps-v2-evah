e7fa09d7d7ef8ed7ab06ce947a8ba103aabb92aa
Marcus Antunes <marcus@evahsaude.com.br>
2023-08-14 18:07:45 -0300

    Correção de endpoint no cadastro de usuário

--- packages/omni_auth/lib/src/modules/register/stores/register_store.dart
+++ packages/omni_auth/lib/src/modules/register/stores/register_store.dart
@@ -5,9 +5,19 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_triple/flutter_triple.dart';
 import 'package:omni_auth/src/modules/register/register_repository.dart';
 import 'package:omni_general/omni_general.dart'
-    show AddressModel, BeneficiaryModel, Formaters, IndividualPersonModel, LecuponRepository, LecuponService, NewBeneficiaryModel, UserModel, ZipCodeStore;
+    show
+        AddressModel,
+        BeneficiaryModel,
+        Formaters,
+        IndividualPersonModel,
+        LecuponRepository,
+        LecuponService,
+        NewBeneficiaryModel,
+        UserModel,
+        ZipCodeStore;
 
-class RegisterStore extends NotifierStore<DioError, NewBeneficiaryModel> with Disposable {
+class RegisterStore extends NotifierStore<DioError, NewBeneficiaryModel>
+    with Disposable {
   final RegisterRepository _repository = Modular.get();
   final ZipCodeStore zipCodeStore = ZipCodeStore();
   final LecuponService lecuponService = LecuponService();
@@ -34,7 +44,8 @@ class RegisterStore extends NotifierStore<DioError, NewBeneficiaryModel> with Di
           ..termsAccepted = state.termsAccepted
           ..programCode = beneficiary.programCode ?? state.programCode
           ..responsible = beneficiary.responsible ?? state.responsible
-          ..individualPerson = beneficiary.individualPerson ?? state.individualPerson,
+          ..individualPerson =
+              beneficiary.individualPerson ?? state.individualPerson,
       );
       setLoading(false);
     }).catchError((onError) {
@@ -91,10 +102,14 @@ class RegisterStore extends NotifierStore<DioError, NewBeneficiaryModel> with Di
     try {
       switch (page) {
         case 0:
-          final bool isDisable = (state.individualPerson!.birth == null || state.individualPerson!.birth!.isEmpty) ||
-              (state.individualPerson!.cpf == null || state.individualPerson!.cpf!.isEmpty) ||
-              (state.individualPerson!.name == null || state.individualPerson!.name!.isEmpty) ||
-              (state.individualPerson!.phone == null || state.individualPerson!.phone!.isEmpty) ||
+          final bool isDisable = (state.individualPerson!.birth == null ||
+                  state.individualPerson!.birth!.isEmpty) ||
+              (state.individualPerson!.cpf == null ||
+                  state.individualPerson!.cpf!.isEmpty) ||
+              (state.individualPerson!.name == null ||
+                  state.individualPerson!.name!.isEmpty) ||
+              (state.individualPerson!.phone == null ||
+                  state.individualPerson!.phone!.isEmpty) ||
               state.individualPerson!.genre == null;
           return isDisable;
         case 1:
@@ -105,31 +120,45 @@ class RegisterStore extends NotifierStore<DioError, NewBeneficiaryModel> with Di
             if (state.responsible == null) {
               isUnderageValid = true;
             } else {
-              isUnderageValid = (state.responsible!.name == null || state.responsible!.name!.isEmpty) ||
-                  (state.responsible!.cpf == null || state.responsible!.cpf!.isEmpty) ||
+              isUnderageValid = (state.responsible!.name == null ||
+                      state.responsible!.name!.isEmpty) ||
+                  (state.responsible!.cpf == null ||
+                      state.responsible!.cpf!.isEmpty) ||
                   (state.responsible!.type == null);
             }
           } else {
             isUnderageValid = false;
           }
-          final bool isDisable = (state.individualPerson!.name == null || state.individualPerson!.name!.isEmpty) ||
-              (state.individualPerson!.phone == null || state.individualPerson!.phone!.isEmpty) ||
+          final bool isDisable = (state.individualPerson!.name == null ||
+                  state.individualPerson!.name!.isEmpty) ||
+              (state.individualPerson!.phone == null ||
+                  state.individualPerson!.phone!.isEmpty) ||
               isUnderageValid;
 
           return isDisable;
         case 3:
-          final bool isDisable = (state.individualPerson!.address!.zipCode == null || state.individualPerson!.address!.zipCode!.isEmpty) ||
-              (state.individualPerson!.address!.city == null || state.individualPerson!.address!.city!.isEmpty) ||
-              (state.individualPerson!.address!.state == null || state.individualPerson!.address!.state!.isEmpty) ||
-              (state.individualPerson!.address!.street == null || state.individualPerson!.address!.street!.isEmpty) ||
-              (state.individualPerson!.address!.district == null || state.individualPerson!.address!.district!.isEmpty);
+          final bool isDisable =
+              (state.individualPerson!.address!.zipCode == null ||
+                      state.individualPerson!.address!.zipCode!.isEmpty) ||
+                  (state.individualPerson!.address!.city == null ||
+                      state.individualPerson!.address!.city!.isEmpty) ||
+                  (state.individualPerson!.address!.state == null ||
+                      state.individualPerson!.address!.state!.isEmpty) ||
+                  (state.individualPerson!.address!.street == null ||
+                      state.individualPerson!.address!.street!.isEmpty) ||
+                  (state.individualPerson!.address!.district == null ||
+                      state.individualPerson!.address!.district!.isEmpty);
 
           return isDisable;
         case 4:
-          final bool isDisable = (state.individualPerson!.user!.username == null || state.individualPerson!.user!.username!.isEmpty) ||
-              (state.individualPerson!.user!.email == null || state.individualPerson!.user!.email!.isEmpty) ||
-              (state.individualPerson!.user!.password == null || state.individualPerson!.user!.password!.isEmpty) ||
-              !state.termsAccepted;
+          final bool isDisable =
+              (state.individualPerson!.user!.username == null ||
+                      state.individualPerson!.user!.username!.isEmpty) ||
+                  (state.individualPerson!.user!.email == null ||
+                      state.individualPerson!.user!.email!.isEmpty) ||
+                  (state.individualPerson!.user!.password == null ||
+                      state.individualPerson!.user!.password!.isEmpty) ||
+                  !state.termsAccepted;
           return isDisable;
 
         default:
