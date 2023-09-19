be867899cc0811a5e05d3a5d9cccd8c3ddf348d5
Marcus Antunes <marcus@evahsaude.com.br>
2023-09-19 16:31:29 -0300

    adicionado webView da EuSaude

--- plugins/omni_core/lib/src/app/modules/urgency_teleattendance/teleattendance_page.dart
+++ plugins/omni_core/lib/src/app/modules/urgency_teleattendance/teleattendance_page.dart
@@ -1,5 +1,11 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_triple/flutter_triple.dart';
+import 'package:omni_core/src/app/modules/urgency_teleattendance/teleattendance_store.dart';
 import 'package:omni_core/src/app/shared/widgets/rating_dialog/rating_dialog.dart';
 import 'package:omni_general/omni_general.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -15,80 +21,85 @@ class TeleattendancePage extends StatefulWidget {
 }
 
 class _TeleattendancePageState extends State<TeleattendancePage> {
-  late WebViewController _webViewController;
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+  final TeleattendanceStore store = TeleattendanceStore();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: NavBarWidget(title: widget.moduleName!).build(context) as AppBar,
       extendBodyBehindAppBar: true,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Você está saindo do app',
-              style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                    fontSize: 22,
-                  ),
-              textAlign: TextAlign.center,
+      body: TripleBuilder<TeleattendanceStore, DioError, bool>(
+        store: store,
+        builder: (_, triple) {
+          if (triple.isLoading) {
+            return const LoadingWidget();
+          }
+          if (triple.event == TripleEvent.error) {
+            return Center(
+              child: SingleChildScrollView(
+                clipBehavior: Clip.antiAlias,
+                physics: const BouncingScrollPhysics(),
+                child: RequestErrorWidget(
+                  message: triple.error!.response!.data['error'].toString(),
+                  buttonText: 'Fechar',
+                  // onPressed: () async {
+                  //   await store.getPharmaOrganizationsList(
+                  //     categoryId: widget.categoryParam,
+                  //   );
+                  // },
+                ),
+              ),
+            );
+          }
+          return SafeArea(
+            child: SizedBox(
+              child: WebView(
+                initialUrl: store.webViewUrl,
+                zoomEnabled: false,
+                javascriptMode: JavascriptMode.unrestricted,
+                onWebViewCreated: (WebViewController webViewController) {
+                  _controller.complete(webViewController);
+                },
+                onProgress: (int progress) {
+                  log('WebView is loading (progress : $progress%)');
+                },
+                javascriptChannels: <JavascriptChannel>{
+                  // _toasterJavascriptChannel(_),
+                }.toSet(),
+                navigationDelegate: (NavigationRequest request) async {
+                  if (request.url == store.webViewUrl) {
+                    return NavigationDecision.navigate;
+                  }
+                  if (await canLaunchUrl(Uri.parse(request.url))) {
+                    await launchUrl(Uri.parse(request.url));
+                  } else {
+                    // Helpers.showDialog(
+                    //   context,
+                    //   RequestErrorWidget(
+                    //     // message: MediktorLabels.mediktorDiagnosisError,
+                    //     // buttonText: MediktorLabels.close,
+                    //     message: 'Error',
+                    //     buttonText: 'Fechar',
+                    //     onPressed: () => Modular.to.pop(),
+                    //   ),
+                    // );
+                  }
+                  return NavigationDecision.prevent;
+                },
+                onPageStarted: (String url) {
+                  log('Page started loading: $url');
+                },
+                onPageFinished: (String url) {
+                  log('Page finished loading: $url');
+                },
+                gestureNavigationEnabled: true,
+              ),
             ),
-            const SizedBox(height: 15),
-            Text(
-              'Você será redirecionado para o atendimento via whatsapp',
-              style: Theme.of(context).textTheme.bodyLarge,
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 15),
-            DefaultButtonWidget(
-              onPressed: () async {
-                await launchUrl(
-                  Uri.parse('https://wa.me/message/U3FWBVIFTGLKP1'),
-                  mode: LaunchMode.externalApplication,
-                ).then((value) async {
-                  await Helpers.showDialog(
-                    context,
-                    RatingDialog(module: 'pronto_atendimento_virtual'),
-                  );
-                  Modular.to.pop();
-                });
-              },
-              text: 'Entendo e concordo',
-            ),
-            const SizedBox(height: 10),
-            DefaultButtonWidget(
-              onPressed: () => Modular.to.pop(),
-              text: 'Voltar',
-              buttonType: DefaultButtonType.outline,
-            ),
-          ],
-        ),
+          );
+        },
       ),
-      // body: WebView(
-      //   initialUrl:
-      //       'https://eusaude.com.vc/b2b_evah/?alx777-get-user-dna=%F0%9F%91%A4%E2%80%89Juarez%E2%A0%80%E2%98%8E%EF%B8%8F%E2%80%8931988623421%E2%A0%80%20%E2%80%8983469014027%E2%A0%80%F0%9F%93%A7%E2%80%89juarez.henrique@eusaude.com.br%E2%A0%80%F0%9F%8F%A5%E2%80%89EUSAUDE%E2%A0%80%F0%9F%94%B0%E2%80%89TRIMESTRAL',
-      //   zoomEnabled: false,
-      //   javascriptMode: JavascriptMode.unrestricted,
-      //   javascriptChannels: {
-      //     JavascriptChannel(
-      //       name: 'flutterChannel',
-      //       onMessageReceived: (JavascriptMessage message) {
-      //         // Captura a mensagem emitida pelo JavaScript
-      //         final String data = message.message;
-      //         print('Evento do JavaScript: $data');
-      //       },
-      //     ),
-      //   },
-      //   onWebViewCreated: (controller) {
-      //     _webViewController = controller;
-      //   },
-      // ),
-      // body: const WebviewScaffold(
-      //   url: 'https://eusaude.com.vc/b2b_evah/?alx777-get-user-dna',
-      //   hidden: true, // Opcional: oculta a webview até que ela esteja carregada
-      // ),
     );
   }
 }
